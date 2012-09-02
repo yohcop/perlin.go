e3fb4d989c2b4a9a11a5ef7d8b54274b0c697abf
Yohann Coppel <yc@leppoc.net>
2012-09-02 02:13:38 -0700

    Add generation of multiple tiles at once, and output basic html file

--- src/perlin/tools/gen_image/gen_image.go
+++ src/perlin/tools/gen_image/gen_image.go
@@ -23,7 +23,13 @@ var cx = flag.Int("cx", 0, "Chunk X")
 var cy = flag.Int("cy", 0, "Chunk Y (only for 2d and 3d)")
 var cz = flag.Int("cz", 0, "Chunk Z (only for 3d)")
 
-var img = flag.String("img", "", "Output image")
+var dx = flag.Int("dx", 1, "Number of tiles to generate in X, starts at -cx.")
+var dy = flag.Int("dy", 1, "Number of tiles to generate in y, starts at -cy.")
+var dz = flag.Int("dz", 1, "Number of tiles to generate in z, starts at -cz.")
+
+var img = flag.String("img", "", "Output image prefix. " +
+                                 "Will be built with _x_y_z.png suffix. " +
+                                 "Only for 1 and 2 d")
 
 // To plot with gnuplot: pipe output to
 // 1d:
@@ -36,40 +42,40 @@ func main() {
   flag.Parse()
   rand.Seed(*seed)
 
+  html := ""
+
   if *d == 1 {
-    out := perlin.Noise1d(*x, *cx, float32(*persist), *from, 1 + *to, perlin.TileNoise)
-    /*
-    for i, x := range out {
-      fmt.Printf("%d %f\n", i, x)
-    }
-    */
-    if len(*img) > 0 {
-      f, err := os.OpenFile(*img, os.O_WRONLY | os.O_CREATE | os.O_TRUNC, 0600)
-      defer f.Close()
-      if err == nil {
-        png.Encode(f, perlin.NewNoise1dImage(*x, *y, out))
-      } else {
-        fmt.Println(err.Error())
+    for ccx := *cx; ccx < *cx + *dx; ccx++ {
+      out := perlin.Noise1d(*x, ccx, float32(*persist), *from, 1 + *to, perlin.TileNoise)
+      if len(*img) > 0 {
+        filename := fmt.Sprintf("%s_%d.png", *img, ccx)
+        html += fmt.Sprintf("<img src=\"%s\">", filename)
+        f, err := os.OpenFile(filename, os.O_WRONLY | os.O_CREATE | os.O_TRUNC, 0600)
+        defer f.Close()
+        if err == nil {
+          png.Encode(f, perlin.NewNoise1dImage(*x, *y, out))
+        } else {
+          fmt.Println(err.Error())
+        }
       }
     }
   } else if *d == 2 {
-    out := perlin.Noise2d(*x, *y, *cx, *cy, float32(*persist), *from, 1 + *to, perlin.TileNoise2d)
-    /*
-    for x := range out {
-      for y, v := range out[x] {
-        fmt.Printf("%d %d %f\n", x, y, v)
+    for ccy := *cy; ccy < *cy + *dy; ccy++ {
+      for ccx := *cx; ccx < *cx + *dx; ccx++ {
+        out := perlin.Noise2d(*x, *y, ccx, ccy, float32(*persist), *from, 1 + *to, perlin.TileNoise2d)
+        if len(*img) > 0 {
+          filename := fmt.Sprintf("%s_%d_%d.png", *img, ccx, ccy)
+          html += fmt.Sprintf("<img src=\"%s\">", filename)
+          f, err := os.OpenFile(filename, os.O_WRONLY | os.O_CREATE | os.O_TRUNC, 0600)
+          defer f.Close()
+          if err == nil {
+            png.Encode(f, perlin.NewNoise2dImage(*x, *y, out))
+          } else {
+            fmt.Println(err.Error())
+          }
+        }
       }
-      fmt.Println("")
-    }
-    */
-    if len(*img) > 0 {
-      f, err := os.OpenFile(*img, os.O_WRONLY | os.O_CREATE | os.O_TRUNC, 0600)
-      defer f.Close()
-      if err == nil {
-        png.Encode(f, perlin.NewNoise2dImage(*x, *y, out))
-      } else {
-        fmt.Println(err.Error())
-      }
+      html += "<br>"
     }
   } else if *d == 3 {
     out := perlin.Noise3d(*x, *y, *z, *cx, *cy, *cz, float32(*persist), *from, 1 + *to, perlin.TileNoise3d)
@@ -81,6 +87,15 @@ func main() {
           }
         }
       }
+    }
+  }
+
+  if len(*img) > 0 && len(html) > 0 {
+    filename := fmt.Sprintf("%s.html", *img)
+    f, err := os.OpenFile(filename, os.O_WRONLY | os.O_CREATE | os.O_TRUNC, 0600)
+    defer f.Close()
+    if err == nil {
+      f.Write([]byte(html))
     }
   }
 }
